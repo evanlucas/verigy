e28b263bfaa14cf840e6a80fba6aea9af2dcd3c7
Evan Lucas <evan@btc.com>
2014-11-07 11:43:24 -0600

    Add bin script

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
 
 var fs = require('fs')
+  , args = process.argv.splice(2)
+
+if (!args.length) {
+  return usage(1)
+}
+
+var email = args.shift()
+
+var helps = ['--help', '-h', '-H']
+
+if (~helps.indexOf(email)) {
+  return usage(0)
+}
+
+var verigy = require('../')
+
+var out = verigy(email)
+
+if (!out) {
+  console.error('not valid:', email)
+  process.exit(1)
+}
+
+console.log(email, '=>', out)
 
 function usage(code) {
   var rs = fs.createReadStream(__dirname + '/usage.txt')
