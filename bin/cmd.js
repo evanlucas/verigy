01fc40ec7a12478690cc67731e2bd31edfb4b232
Evan Lucas <evan@btc.com>
2014-11-07 11:33:09 -0600

    Initial commit

---
+++ bin/cmd.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+
+function usage(code) {
+  var rs = fs.createReadStream(__dirname + '/usage.txt')
+  rs.pipe(process.stdout)
+  rs.on('close', function() {
+    if (code) process.exit(code)
+  })
+}
---
+++ test/test.js
@@ -0,0 +1,44 @@
+var should = require('should')
+  , verigy = require('../')
+
+describe('verigy', function() {
+  it('should return email if regular email', function() {
+    var email = 'evan@gmail.com'
+    var o = verigy(email)
+    o.should.equal(email)
+  })
+
+  it('should strip out periods', function() {
+    var email = 'evan.lucas@gmail.com'
+    var o = verigy(email)
+    o.should.equal('evanlucas@gmail.com')
+  })
+
+  it('should strip out correct username', function() {
+    var email = 'evanlucas+blah@gmail.com'
+    var o = verigy(email)
+    o.should.equal('evanlucas@gmail.com')
+  })
+
+  it('should strip out periods and correct username', function() {
+    var email = 'evan.lucas+blah@gmail.com'
+    var o = verigy(email)
+    o.should.equal('evanlucas@gmail.com')
+  })
+
+  it('should return null if !email', function() {
+    var o = verigy(null)
+    should.not.exist(o)
+  })
+
+  it('should return null if more than one @', function() {
+    var email = 'evan.lucas@blah@gmail.com'
+    var o = verigy(email)
+    should.not.exist(o)
+  })
+
+  it('should return null if no @', function() {
+    var o = verigy('evanlucas')
+    should.not.exist(o)
+  })
+})
